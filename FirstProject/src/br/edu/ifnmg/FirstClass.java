fca1e9f3a5fc2b839c37df5e7e8af2bc57b6dfd3
guisso <guisso@guisso-VirtualBox>
2018-04-24 21:31:20 -0300

    Alteração de comentário, inclusão de propriedade id e respectivos getters e setters

--- FirstProject/src/br/edu/ifnmg/FirstClass.java
+++ FirstProject/src/br/edu/ifnmg/FirstClass.java
@@ -1,9 +1,18 @@
 package br.edu.ifnmg;
 
 /**
- * Classe para testes
+ * Classe para testes isto é uma alteração
  * @author guisso
  */
 public class FirstClass {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
     
 }
